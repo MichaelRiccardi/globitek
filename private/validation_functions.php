d3790be7653fb4c9d12d4ee4aefdd5ceca9746aa
Michael Riccardi <michaelriccardi@ufl.edu>
2017-01-07 00:08:11 -0500

    Registration form and required validation

--- private/validation_functions.php
+++ private/validation_functions.php
@@ -1,18 +1,102 @@
 <?php
 
-  // is_blank('abcd')
-  function is_blank($value='') {
-    // TODO
-  }
+    $max_length = 255;
 
-  // has_length('abcd', ['min' => 3, 'max' => 5])
-  function has_length($value, $options=array()) {
-    // TODO
-  }
+    $name_length = array(
+        "min" => 2,
+        "max" => $max_length
+    );
 
-  // has_valid_email_format('test@test.com')
-  function has_valid_email_format($value) {
-    // TODO
-  }
+    $email_length = array(
+        "max" => $max_length
+    );
+
+    $username_length = array(
+        "min" => 8,
+        "max" => $max_length
+    );
+
+    // is_blank('abcd')
+    function is_blank($value='') {
+        return (strlen($value) == 0);
+    }
+
+    // has_length('abcd', ['min' => 3, 'max' => 5])
+    function has_length($value, $options=array()) {
+        
+        $length = strlen($value);
+        
+        $meetsMin = !isset($options['min']) || $length >= $options['min'];
+        $meetsMax = !isset($options['max']) || $length <= $options['max'];     
+        
+        return $meetsMin && $meetsMax;
+        
+    }
+
+    // has_valid_email_format('test@test.com')
+    function has_valid_email_format($value) {
+        // TODO
+        return (strpos($value, "@") !== false);
+    }
+
+    function validate_first_name($first_name, &$errors)
+    {    
+        global $name_length;
+        
+        if(is_blank($first_name))
+        {
+            $errors[] = "First name cannot be blank.";
+        }
+        else if(!has_length($first_name, $name_length))
+        {
+            $errors[] = "First name must be between {$name_length['min']} and {$name_length['max']} characters.";
+        }
+    }
+
+    function validate_last_name($last_name, &$errors)
+    {    
+        global $name_length;
+        
+        if(is_blank($last_name))
+        {
+            $errors[] = "Last name cannot be blank.";
+        }
+        else if(!has_length($last_name, $name_length))
+        {
+            $errors[] = "Last name must be between {$name_length['min']} and {$name_length['max']} characters.";
+        }
+    }
+
+    function validate_email($email, &$errors)
+    {
+        global $email_length;
+        
+        if(is_blank($email))
+        {
+            $errors[] = "Email cannot be blank.";
+        }
+        else if(!has_valid_email_format($email))
+        {
+            $errors[] = "Email must be a valid format.";
+        }
+        else if(!has_length($email, $email_length))
+        {
+            $errors[] = "Email must not exceed {$email_length['max']} characters.";
+        }
+    }
+
+    function validate_username($username, &$errors)
+    {
+        global $username_length;
+        
+        if(is_blank($username))
+        {
+            $errors[] = "Username cannot be blank.";
+        }
+        else if(!has_length($username, $username_length))
+        {
+            $errors[] = "Username must be between {$username_length['min']} and {$username_length['max']} characters.";
+        }           
+    }
 
 ?>
--- public/register.php
+++ public/register.php
@@ -1,35 +1,52 @@
 <?php
-  require_once('../private/initialize.php');
+    require_once('../private/initialize.php');
 
-  // Set default values for all variables the page needs.
+    // Set default values for all variables the page needs.
+    $first_name = "";
+    $last_name = "";
+    $email = "";
+    $username = "";
+    $errors = array();
 
-  // if this is a POST request, process the form
-  // Hint: private/functions.php can help
+    // if this is a POST request, process the form
+    // Hint: private/functions.php can help
 
-    // Confirm that POST values are present before accessing them.
+    if(is_post_request())
+    {        
+        // Confirm that POST values are present before accessing them.
+        $first_name = isset($_POST['first_name']) ? $_POST['first_name'] : "";
+        $last_name = isset($_POST['last_name']) ? $_POST['last_name'] : "";
+        $email = isset($_POST['email']) ? $_POST['email'] : "";
+        $username = isset($_POST['username']) ? $_POST['username'] : "";
+        
+        // Perform Validations        
+        validate_first_name($first_name, $errors);
+        validate_last_name($last_name, $errors);
+        validate_email($email, $errors);
+        validate_username($username, $errors);
 
-    // Perform Validations
-    // Hint: Write these in private/validation_functions.php
+        // if there were no errors, submit data to database
+        if(empty($errors)) 
+        {
+            // Write SQL INSERT statement
+            $sql = "";
 
-    // if there were no errors, submit data to database
+            // For INSERT statments, $result is just true/false
+            // $result = db_query($db, $sql);
+            // if($result) {
+            //   db_close($db);
 
-      // Write SQL INSERT statement
-      // $sql = "";
+            //   TODO redirect user to success page
 
-      // For INSERT statments, $result is just true/false
-      // $result = db_query($db, $sql);
-      // if($result) {
-      //   db_close($db);
-
-      //   TODO redirect user to success page
-
-      // } else {
-      //   // The SQL INSERT statement failed.
-      //   // Just show the error, not the form
-      //   echo db_error($db);
-      //   db_close($db);
-      //   exit;
-      // }
+            // } else {
+            //   // The SQL INSERT statement failed.
+            //   // Just show the error, not the form
+            //   echo db_error($db);
+            //   db_close($db);
+            //   exit;
+            // }
+        }       
+    }
 
 ?>
 
@@ -41,11 +58,22 @@
   <p>Register to become a Globitek Partner.</p>
 
   <?php
-    // TODO: display any form errors here
-    // Hint: private/functions.php can help
+    
+    // DONE: display any form errors here
+    echo display_errors($errors);
+    
   ?>
 
-  <!-- TODO: HTML form goes here -->
+  <!-- DONE: HTML form goes here -->
+    
+    <form action="" method="post">
+        
+        <p><b>First Name: </b><input type="text" name="first_name" value="<?php echo $first_name; ?>"></p>
+        <p><b>Last Name: </b><input type="text" name="last_name" value="<?php echo $last_name; ?>"></p>
+        <p><b>Email: </b><input type="text" name="email" value="<?php echo $email; ?>"></p>
+        <p><b>Username: </b><input type="text" name="username" value="<?php echo $username; ?>"></p>
+        <p><input type="submit" value="Submit Registration"></p>
+    </form>
 
 </div>
 
